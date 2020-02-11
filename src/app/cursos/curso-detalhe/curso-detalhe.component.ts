b919583e7475ef3a6fde20820c47c4045740f57b
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-02-11 21:56:06 -0200

    Criando um exemplo utilizando event-binding

--- src/app/cursos/curso-detalhe/curso-detalhe.component.ts
+++ src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoDetalheComponent implements OnInit {
 
+  valorAtual: string = '';
+  valorSalvo = '';
+  isMouseOver: boolean = false;
+  nomeDoCurso: string = 'Angular';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
